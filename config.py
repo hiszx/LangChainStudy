aad50f650b4e129b06daba911fd927dcff2b85b6
yz <260198384@qq.com>
2024-04-07 14:39:28 +0800

    okokok

---
+++ config.py
@@ -0,0 +1,11 @@
+from langchain.chat_models import AzureChatOpenAI
+OPENAI_API_TYPE = "azure"
+OPENAI_API_BASE = "https://nova-openai.openai.azure.com/"
+OPENAI_API_KEY="7a1ce118af3e418fbe41a7625a200405"
+OPENAI_API_VERSION = "2023-05-15"
+
+#HTML CONFIG
+bot_template=1
+
+
+user_template =2
